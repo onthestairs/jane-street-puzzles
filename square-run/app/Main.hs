005c6f166bb96de992eff0de6893557d253ee7af
Austin Platt <austinplatt@gmail.com>
2017-09-07 12:00:06 +0100

    tidy square run

--- square-run/app/Main.hs
+++ square-run/app/Main.hs
@@ -3,24 +3,18 @@
 module Main where
 
 import Linear.V2
-import Linear.V
 import Data.Function.Memoize
-import Debug.Trace
 import Control.Applicative
 import Data.Ord (comparing)
 import Data.Foldable (maximumBy)
 import Data.Bifunctor
+import Data.List (intercalate)
 
 type Board = [[Int]]
 type Coord = V2 Int
 type Move = Coord -> Coord
 
--- deriveMemoizable ''Matrix
 deriveMemoizable ''V2
--- deriveMemoizable ''V
-
--- getElem' :: Matrix a -> Coord -> a
--- getElem' m (V2 row col) = getElem row col m
 
 getElem' :: Board -> Coord -> Int
 getElem' m (V2 row col) = (m !! (row - 1)) !! (col - 1)
@@ -80,54 +74,6 @@ isGoodBoard :: Board -> Bool
 isGoodBoard b =
   any (> (-20)) (concat b)
 
--- solve :: Board -> Coord -> Int
--- solve b c =
---   maximum $ concatMap next (queenMoves c)
---   where
---     next :: Move -> [Int]
---     next m = maxNext ++ finalValue
---       where nextCoord :: Coord
---             nextCoord = m c
---             nextBoard :: Board
---             nextBoard = reduceBoardAfterMove b c nextCoord
---             value :: Int
---             value = getElem' b nextCoord
---             isGoodNextBoard :: Bool
---             isGoodNextBoard = isGoodBoard nextBoard
---             maxNext :: [Int]
---             maxNext = if isGoodNextBoard then [value + solve nextBoard nextCoord] else []
---             finalValue :: [Int]
---             finalValue = if nextCoord == endCoord then [value] else []
-
---
--- solve :: (Board -> Coord -> Maybe Int) -> Board -> Coord -> Maybe Int
--- solve f b c =
--- maximum $ map next (queenMoves c)
--- where
--- next :: Move -> SpaceAnswer
--- next m = traceShow (c, nextCoord, value, best, isGoodNextBoard) $ best
--- -- next m = best
--- where nextCoord :: Coord
--- nextCoord = m c
--- nextBoard :: Board
--- nextBoard = reduceBoardAfterMove b c nextCoord
--- value :: Int
--- value = getElem' b nextCoord
--- isGoodNextBoard :: Bool
--- isGoodNextBoard = isGoodBoard nextBoard
--- maxNext :: EndSpace
--- maxNext = if isGoodNextBoard then (+ value) <$> f nextBoard nextCoord else Nothing
--- finalValue :: CurrentSpace
--- finalValue = if nextCoord == endCoord then Just value else Nothing
--- best :: SpaceAnswer
--- best = justMax finalValue maxNext
-
-justMax :: Ord a => Maybe a -> Maybe a -> Maybe a
-justMax (Just a) (Just b) = Just (max a b)
-justMax (Just a) Nothing = Just a
-justMax Nothing (Just b) = Just b
-justMax Nothing Nothing = Nothing
-
 type CurrentSpace = Maybe (Int, [Coord])
 type EndSpace = Maybe (Int, [Coord])
 type SpaceAnswer = Maybe (Int, [Coord])
@@ -137,24 +83,14 @@ solve f b c =
   maximumBy (comparing (fmap fst)) $ map next (queenMoves c)
   where
     next :: Move -> SpaceAnswer
-    -- next m = traceShow (c, nextCoord, value, best, isGoodNextBoard) $ best
     next m = best
-      where nextCoord :: Coord
-            nextCoord = m c
-            nextBoard :: Board
+      where nextCoord = m c
             nextBoard = reduceBoardAfterMove b c nextCoord
-            value :: Int
             value = getElem' b nextCoord
-            isGoodNextBoard :: Bool
             isGoodNextBoard = isGoodBoard nextBoard
-            nextAnswer :: SpaceAnswer
             nextAnswer = f nextBoard nextCoord
-            maxNext :: EndSpace
             maxNext = if isGoodNextBoard then (bimap (+ value) ((:) c)) <$> nextAnswer else Nothing
-            final :: CurrentSpace
-            final = if nextCoord == endCoord then Just (value, [c]) else Nothing
-            best :: SpaceAnswer
-            -- bestValue = justMaxBy final maxNext
+            final = if nextCoord == endCoord then Just (value, [c, endCoord]) else Nothing
             best = maximumBy (comparing (fmap fst)) $ [final, maxNext]
 
 solve' = memoFix2 solve
@@ -176,11 +112,13 @@ board = [
   [21, 20, 28,  4,  9, 26,  7, 14],
   [ 1,  6,  9, 19, 29, 10, 16,  0]]
 
+prettyCoord :: Coord -> String
 prettyCoord (V2 row col) =
   (['a'..'h'] !! (col - 1)) : show (9 - row)
 
-prettyPath cs = map prettyCoord cs
+prettyPath :: [Coord] -> String
+prettyPath cs = intercalate " -> " (map prettyCoord cs)
 
 main :: IO ()
-main = print (v, prettyPath cs)
+main = putStrLn ("Max score: " ++ show v ++ ", Path: " ++ prettyPath cs)
   where Just (v, cs) = solve' board startCoord
